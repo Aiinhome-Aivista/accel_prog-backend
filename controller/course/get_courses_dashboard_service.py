84502326be410055d0d9a69c27c2ff77f67ea160
Sonia Khatun <sonia2khatun4@gmail.com>
2026-04-08 10:40:55 +0530

    browse more courses by user

--- controller/course/get_courses_dashboard_service.py
+++ controller/course/get_courses_dashboard_service.py
@@ -1,5 +1,45 @@
+# from config import get_db_connection
+# from flask import jsonify
+
+
+# def get_courses_dashboard_service():
+#     conn = None
+#     cur = None
+
+#     try:
+#         conn = get_db_connection()
+#         cur = conn.cursor()
+
+        
+#         cur.execute("CALL course.get_all_courses_dashboard(%s)", (None,))
+
+#         row = cur.fetchone()
+
+#         if row is None:
+#             result = []
+#         elif isinstance(row, dict):
+#             first_key = next(iter(row), None)
+#             result = row[first_key] if first_key is not None else row
+#         else:
+#             result = row[0]
+
+#         conn.commit()
+
+#         return jsonify({"status": "success", "data": result}), 200
+
+#     except Exception as e:
+#         return jsonify({"status": "error", "message": str(e)}), 500
+
+#     finally:
+#         if cur:
+#             cur.close()
+#         if conn:
+#             conn.close()
+
+
+
 from config import get_db_connection
-from flask import jsonify
+from flask import jsonify, request
 
 
 def get_courses_dashboard_service():
@@ -7,11 +47,24 @@ def get_courses_dashboard_service():
     cur = None
 
     try:
+        #  args theke user_id nebo
+        user_id = request.args.get("user_id", type=int)
+
+        if not user_id:
+            return jsonify({
+                "status": "fail",
+                "message": "user_id is required",
+                "data": []
+            }), 400
+
         conn = get_db_connection()
         cur = conn.cursor()
 
-        
-        cur.execute("CALL course.get_all_courses_dashboard(%s)", (None,))
+        #  updated CALL (IN + OUT)
+        cur.execute(
+            "CALL course.get_all_courses_dashboard_v1(%s, %s)",
+            (user_id, None)
+        )
 
         row = cur.fetchone()
 
@@ -25,10 +78,16 @@ def get_courses_dashboard_service():
 
         conn.commit()
 
-        return jsonify({"status": "success", "data": result}), 200
+        return jsonify({
+            "status": "success",
+            "data": result
+        }), 200
 
     except Exception as e:
-        return jsonify({"status": "error", "message": str(e)}), 500
+        return jsonify({
+            "status": "error",
+            "message": str(e)
+        }), 500
 
     finally:
         if cur:
